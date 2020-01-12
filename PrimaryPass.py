c42eaccb7e58bd1e7cf9df08e851442b83e208fe
Agere <bmateusz1998@gmail.com>
2020-01-12 22:34:13 +0100

    Create PrimaryPass.py

---
+++ PrimaryPass.py
@@ -0,0 +1,12 @@
+# import Adafruit_BBIO.ADC as ADC
+
+# analog = 'P9_33'
+
+# def read_voltage():
+#     return ADC.read(analog) * 1.8
+
+# def init():
+#     ADC.setup()
+
+def read():
+    return input('PIN 1: ')
