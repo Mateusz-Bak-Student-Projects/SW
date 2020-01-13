fc9df7f3da150b12520562b5aa3cfe6ddeafa357
Agere <bmateusz1998@gmail.com>
2020-01-13 21:27:36 +0100

    Add button handling

--- SafeLock.py
+++ SafeLock.py
@@ -1,15 +1,21 @@
 import Display
 # import Adafruit_BBIO.PWM as PWM
+# import Adafruit_BBIO.GPIO as GPIO
 
 # servo = 'P9_14'
+# button = 'P8_12'
 locked = { 0 : False }
 
 # def set_angle(angle):
 #     dutyCycle = 0.05 * angle + 3
 #     PWM.set_duty_cycle(servo, dutyCycle)
 
+# def get_button():
+#     return GPIO.input(button) == 1
+
 # def init():
 #     PWM.start(servo, 3, 50)
+#     GPIO.setup(button, GPIO.IN)
 
 def open(id):
     # set_angle(90)
@@ -22,4 +28,5 @@ def close(id):
     Display.write('Locked\n\r')
 
 def is_open(id):
+    # return not get_button()
     return not locked[id]
