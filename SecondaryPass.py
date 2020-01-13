c115851eae5a82b9be66dc85233411d3db5c66b7
Agere <bmateusz1998@gmail.com>
2020-01-13 20:49:14 +0100

    Add secondary passcode input procedure

--- SecondaryPass.py
+++ SecondaryPass.py
@@ -1,10 +1,14 @@
+import numpy as np
+from bisect import bisect
 # import Adafruit_BBIO.GPIO as GPIO
 # import time
 
-# trigger = 'P9_16'
-# echo = 'P9_41'
+# triggerPins = ['P9_16']
+# echoPins = ['P9_41']
 
-# def read_distance():
+# def read_value(index):
+#    trigger = triggerPins[index]
+#    echo = echoPins[index]
 #    GPIO.output(trigger, True)
 #    time.sleep(0.00001)
 #    GPIO.output(trigger, False)
@@ -18,8 +22,34 @@
 #    return distance
 
 # def init():
-#     GPIO.setup(trigger, GPIO.OUT)
-#     GPIO.setup(echo, GPIO.IN)
+#     for trigger, echo in zip(triggerPins, echoPins):
+#         GPIO.setup(trigger, GPIO.OUT)
+#         GPIO.setup(echo, GPIO.IN)
+
+# def read():
+#     passcodeLength = 8
+#     nSegments = 4
+#     maxValue = 40
+#     bounds = np.linspace(0, maxValue, num=(nSegments+1))
+#     nInputs = len(triggerPins)
+#     restart = True
+#     while restart:
+#         for i in range(nInputs):
+#             v = read_value(i)
+#             state = bisect(bounds[1:], v)
+#             if state < nSegments:
+#                 break
+#             if i == nInputs - 1:
+#                 restart = False
+#     states = np.full(nInputs, nSegments)
+#     passcode = ''
+#     while len(passcode) < passcodeLength:
+#         for i, state in enumerate(states):
+#             v = read_value(i)
+#             newState = states[i] = bisect(bounds[1:], v)
+#             if state == nSegments and newState < nSegments:
+#                 passcode += hex(i * nSegments + newState)[2]
+#     return passcode
 
 def read():
     return input('PIN 2: ')
