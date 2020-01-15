399b588c6bbf874fccbb62ddc70489f7052859ad
Agere <bmateusz1998@gmail.com>
2020-01-15 23:17:05 +0100

    Add Arduino RFID scanning functionality

--- ardu.ino
+++ ardu.ino
@@ -1,18 +1,24 @@
 #include <Wire.h>
 #include <SoftwareSerial.h>
 #include <LiquidCrystal_I2C.h>
+#include <SPI.h>
+#include <MFRC522.h>
 
 SoftwareSerial uart(0, 1);
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+MFRC522 mfrc522(10, 9);
 
 void setup()
 {
-    Serial.begin(9600);
+    // Serial.begin(9600);
     uart.begin(9600);
 
     lcd.begin(16, 2);
     lcd.backlight();
-    lcd.setCursor(0, 0);
+    lcd.clear();
+
+    SPI.begin();
+    mfrc522.PCD_Init();
 }
 
 void loop()
@@ -20,6 +26,12 @@ void loop()
     if (uart.available() > 0)
     {
         display(uart.read());
+    }
+    if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial())
+    {
+        String id = String(getID());
+        uart.write(id + '\n');
+        mfrc522.PICC_HaltA();
     }
 }
 
@@ -38,4 +50,15 @@ void display(char c)
         lcd.print(s);
         break;
     }
+}
+
+unsigned int getID()
+{
+    unsigned int id = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        id << 8;
+        id += mfrc255.uid.uidByte[i]
+    }
+    return id;
 }
