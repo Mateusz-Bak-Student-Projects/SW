ea3ff1db87f0e22413dcf20cd29f5d5f4fdeb0d9
Agere <bmateusz1998@gmail.com>
2020-01-17 21:59:50 +0100

    Remove 'with' statement

--- RFIDScanner.py
+++ RFIDScanner.py
@@ -4,19 +4,23 @@
 # def scan_RFID(block=True):
 #     timeout = None if block else 0
 #     UART.setup("UART1")
-#     with serial.Serial(port="/dev/ttyO1", baudrate=9600, timeout=timeout) as ser:
-#         b = ser.read_until('\n')
-#         if len(b) == 0:
-#             return None
-#         else:
-#             ser.timeout = None
-#             s = b.decode('utf-8')
-#             while s[-1] != '\n':
-#                 b = ser.read_until('\n')
-#                 if len(b) == 0:
-#                     return None
-#                 s += b.decode('utf-8')
-#             return int(s[:-1])
+#     ser = serial.Serial(port="/dev/ttyO1", baudrate=9600, timeout=timeout)
+#     ser.close()
+#     ser.open()
+#     b = ser.read_until('\n')
+#     if len(b) == 0:
+#         ser.close()
+#         return None
+#     else:
+#         ser.timeout = None
+#         s = b.decode('utf-8')
+#         while s[-1] != '\n':
+#             b = ser.read_until('\n')
+#             if len(b) == 0:
+#                 return None
+#             s += b.decode('utf-8')
+#         ser.close()
+#         return int(s[:-1])
 
 def scan_RFID(block=True):
     return int(input('RFID: '))
